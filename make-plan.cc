12cb8cb2661c7abd4ac252589799d3cbec4a31fd
Benjamin Yi <bentaebox@gmail.com>
2019-12-16 17:27:23 -0500

    move custom graph class into main file

--- make-plan.cc
+++ make-plan.cc
@@ -16,7 +16,6 @@
 
 #include <iostream>
 #include <fstream>
-#include "customgraph.h"
 #include <math.h>
 #include <libplayerc++/playerc++.h>
 using namespace PlayerCc;  
@@ -34,6 +33,8 @@ const int SIZE = 32; // The number of squares per side of the occupancy grid
  *
  **/
 
+struct Node;
+class Graph;
 void fillMap(int maptext[][SIZE]);
 Node* findPath(int path[256][2], int map[SIZE][SIZE], int init[], int goal[]);
 double manhattanDistance(int x1, int y1, int x2, int y2);
@@ -631,3 +632,91 @@ void fillMap(int maptext[][SIZE]) {
   }
   is.close();
 }
+
+struct Node {
+  int x, y;
+  double cost;
+  struct Node *prev;
+};
+
+class Graph {
+public:
+Graph() : nodes(new Node*[512]), capacity(512), length(0) {}
+void sort() {quicksort(0, length-1);}
+
+void push(int x, int y, double cost, struct Node *n) {
+  struct Node *newnode = new Node;
+  newnode->x = x;
+  newnode->y = y;
+  newnode->cost = cost;
+  newnode->prev = n;
+  nodes[length] = newnode;
+  length += 1;
+}
+
+void push(struct Node *n) {
+  if (length >= 512) {
+    capacity *= 2;
+    Node **tmp = new Node*[capacity];
+    for (int i = 0; i < 512; i++) {
+      tmp[i] = nodes[i];
+    }
+    nodes = tmp;
+    delete [] tmp;
+  }
+  nodes[length] = n;
+  length += 1;
+}
+
+Node * pop() {
+  if (length == 0) throw ("Popped from empty..");
+  length -= 1;
+  return nodes[length];
+}
+
+bool contains(struct Node n) {
+  for (int i = 0; i < length; i++) {
+    if (nodes[i]->x == n.x && nodes[i]->y == n.y) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool isEmpty() {
+  return (length == 0);
+}
+
+private:
+double *costs;
+Node **nodes;
+int capacity;
+int length;
+
+void swap(int i, int j) {
+  Node *tmp = nodes[i];
+  nodes[i] = nodes[j];
+  nodes[j] = tmp;
+}
+
+int partition(int lo, int hi) {
+  double v = nodes[hi]->cost;
+  int i = lo - 1;
+  for (int j = lo; j <= hi-1; j++) {
+    if (nodes[j]->cost >= v) {
+      i++;
+      swap(i, j);
+    }
+  }
+  swap(i+1, hi);
+  return i+1;
+}
+
+void quicksort(int lo, int hi) {
+  if (lo < hi) {
+    int par = partition(lo, hi);
+    quicksort(lo, par-1);
+    quicksort(par+1, hi);
+  }
+}
+};
